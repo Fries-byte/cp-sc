d468dfac041f3949d26ac4bed7a74575a0eb8034
Fries-Byte <69freshfrenchfries@gmail.com>
2025-02-13 03:27:12 -0800

    Update src.py

--- src.py
+++ src.py
@@ -1,30 +1,27 @@
-# =--=--=--=--=--=--=--=--=--=--=--=--=
-# PiStud's compiler source
-# Created by PiStud-Lang (GitHub)
-# and written by Fries-byte (GitHub)
-# Learn more on our website or README.md
-#
-# 2025 - presents | The Programming Language PiStud
-# =--=--=--=--=--=--=--=--=--=--=--=--=
+'''
+ * PiStud's compiler source
+ * Created by PiStud-Lang (GitHub)
+ * and written by Fries-byte (GitHub)
+ * Learn more on our website or README.md
+ *
+ * 2025 - presents | The Programming Language PiStud
+'''
 
 import os
 import urllib.request
 import subprocess
 import sys
+from compiler.modify import url
 
 def modify_file(file_path, url):
     try:
         with urllib.request.urlopen(url) as response:
             new_content = response.read().decode('utf-8')
-
         with open(file_path, 'r') as file:
             original_lines = file.readlines()
-
         original_lines[0] = new_content + '\n'
-
         with open(file_path, 'w') as file:
             file.writelines(original_lines)
-
         return True
     except Exception as e:
         print(f"Error: {e}")
@@ -47,18 +44,12 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: python src.py <target_file>")
         return
-
     target_file = sys.argv[1]
-    url = "https://raw.githubusercontent.com/Fries-byte/pistud/refs/heads/main/src/ps.py"
-
     with open(target_file, 'r') as file:
         original_lines = file.readlines()
-
     if not modify_file(target_file, url):
         return
-
     compile_with_pyinstaller(target_file)
-
     revert_file(target_file, original_lines)
 
 if __name__ == "__main__":
